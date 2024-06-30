501f1d6789afda2d22dfbea3352ad88788eb9081
LuongNV <luong1c11@gmail.com>
2024-06-30 08:56:35 +0700

    remove unused files

--- src/commands.sh
+++
@@ -1,8 +0,0 @@
-aws s3 mb s3://luongnv-code-sam-demo
-
-#package cloudformation
-aws cloudformation package --s3-bucket luongnv-code-sam-demo --template-file template.yaml --output-template-file gen/template-generated.yaml
-
-#sam packge...
-#deploy
-aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
